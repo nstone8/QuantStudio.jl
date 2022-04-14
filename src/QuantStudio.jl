92463c0ca91a9d3101890c89e245b885afbbd618
Nick Stone <stone.nicholas@gmail.com>
2022-04-14 16:21:54 -0500

    Turns out the QS3 doesn't put the 'Well' column first

--- src/QuantStudio.jl
+++ src/QuantStudio.jl
@@ -25,7 +25,7 @@ function Delta2.readpcr(::QS,csvfilename::AbstractString)::QPCRDataset
     startline=missing
     endline=missing
     for line in filelines
-        if ((split(line,",")[1] == "Well") && ismissing(startline)) #only want to take the first line that fits the bill
+        if ("Well" in (split(line,",")) && ismissing(startline)) #only want to take the first line that fits the bill
             #this is the line containing our column names
             startline=linenumber
         end
